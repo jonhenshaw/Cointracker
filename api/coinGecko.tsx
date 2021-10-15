38c5cd92a15a3fa6934077cbfb85a325841bea26
Jon Henshaw <jhensh309@gmail.com>
2021-10-15 00:10:08 -0400

    Complete coin list table

--- api/coinGecko.tsx
+++ api/coinGecko.tsx
@@ -5,7 +5,11 @@ export async function getMarkets() : Promise<any> {
 
 export interface Coin {
   id: string,
+  name: string,
   symbol: string,
-  current_price: string,
-  image: string
+  current_price: number,
+  image: string,
+  market_cap: number,
+  market_cap_rank: number,
+  price_change_percentage_24h: number,
 }
--- components/CoinList.tsx
+++ components/CoinList.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Image, StyleSheet, ScrollView, RefreshControl, FlatList, SafeAreaView, ImageSourcePropType } from 'react-native';
 import { Text, View } from './Themed';
 import { getMarkets, Coin } from '../api/coinGecko';
+import { Line } from 'react-native-svg';
 
 export default function CoinList() {
   const [isLoading, setLoading] = useState(true);
@@ -13,25 +14,81 @@ export default function CoinList() {
   const day = date.toDateString();
   const time = date.toLocaleTimeString();
 
+  const filterAmount = 30;
+
+  function lineBreak() {
+    return (
+      <View
+        style={{
+          borderBottomColor: 'white',
+          borderBottomWidth: 1,
+          marginTop: 10,
+          marginBottom: 10
+        }}
+      />
+    )
+  }
+
+  function renderHeaders() {
+    return (
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{ width: 40, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Text style={styles.symbol}>Coin
+          </Text>
+        </View>
+        <View style={{ width: 90 }}>
+          <Text style={styles.price}>Price
+          </Text>
+        </View>
+        <View style={{ width: 70 }}>
+          <Text style={styles.percentage}>24hr
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.marketCap}>Market Cap
+          </Text>
+        </View>
+      </View>
+    )
+  }
+
   function renderPairs() {
-    var filteredData: any = data.slice(0, 50);
-    return (filteredData.map((pair: Coin) =>
-      <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }} key={pair.id}>
-        <Image style={styles.tinyLogo} source={{
-          uri: pair.image,
-        }}></Image>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          {pair.symbol.toUpperCase()}
-        </Text>
-        <Text
-          style={styles.price}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          {pair.current_price}
-        </Text>
+    return (data.slice(0, filterAmount).map((pair: Coin) =>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{ width: 45, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Image style={styles.tinyLogo} source={{
+            uri: pair.image,
+          }}></Image>
+          <Text
+            style={styles.symbol}
+            lightColor="rgba(0,0,0,0.8)"
+            darkColor="rgba(255,255,255,0.8)">
+            {pair.symbol.toUpperCase()}
+          </Text>
+        </View>
+        <View style={{ width: 90 }}>
+          <Text
+            style={styles.price}
+            lightColor="rgba(0,0,0,0.8)"
+            darkColor="rgba(255,255,255,0.8)">
+            {`$${pair.current_price}`}
+          </Text>
+        </View>
+        <View style={{ width: 70 }}>
+          <Text style={styles.percentage}
+            lightColor={pair.price_change_percentage_24h >= 0 ? 'green' : 'red'}
+            darkColor={pair.price_change_percentage_24h >= 0 ? 'green' : 'red'}>
+            {`${pair.price_change_percentage_24h.toFixed(2)}%`}
+          </Text>
+        </View>
+        <View>
+          <Text
+            style={styles.marketCap}
+            lightColor="rgba(0,0,0,0.8)"
+            darkColor="rgba(255,255,255,0.8)">
+            {`$${Number(pair.market_cap).toLocaleString()}`}
+          </Text>
+        </View>
       </View>
     ))
   }
@@ -48,6 +105,8 @@ export default function CoinList() {
       .finally(() => setLoading(false));
   }
 
+  const isPositive = (number: number): boolean => number >= 0 ? true : false;
+
   useEffect(() => {
     getData();
   }, []);
@@ -60,9 +119,8 @@ export default function CoinList() {
           onRefresh={onRefresh}
         />}
     >
-      <Text style={{ marginBottom: 10 }}>
-        Last updated: {day} {time}
-      </Text>
+      {renderHeaders()}
+      {lineBreak()}
       {renderPairs()}
     </ScrollView>
   );
@@ -75,9 +133,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   tinyLogo: {
-    width: 20,
-    height: 20,
-    marginRight: 5
+    width: 15,
+    height: 15
   },
   getStartedContainer: {
     marginHorizontal: 50,
@@ -89,16 +146,26 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     paddingHorizontal: 4,
   },
-  getStartedText: {
-    fontSize: 17,
+  symbol: {
+    fontSize: 14,
     lineHeight: 24,
     textAlign: 'left',
   },
   price: {
-    fontSize: 17,
+    fontSize: 14,
+    lineHeight: 24,
+    textAlign: 'right'
+  },
+  percentage: {
+    fontSize: 14,
     lineHeight: 24,
     textAlign: 'right',
-    marginLeft: 10
+    marginRight: 10
+  },
+  marketCap: {
+    fontSize: 14,
+    lineHeight: 24,
+    textAlign: 'right',
   },
   helpContainer: {
     marginTop: 15,
