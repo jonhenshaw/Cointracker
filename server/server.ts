275cc54b92ce43909d5ad955ca067071c7730c61
Jon Henshaw <jhensh309@gmail.com>
2021-10-22 14:58:33 -0400

    Renames

--- server/server.ts
+++ server/server.ts
@@ -2,10 +2,17 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
 app.use(cors());
 
 app.use('/login', (req: any, res: any) => {
-  res.send('test123');
+  bcrypt.hash(req.body.password, 10, (err: Error, hash: string) => {
+    console.log(hash)
+  })
+  res.send({token: 'test123'});
 });
 
 app.listen(8080, () => console.log('API is running on http://192.168.0.139:8080/login'));
