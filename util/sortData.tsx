1d4ccf581bbf76f71925d8770516a8ff578c4e11
Yihan Chen <yihanc92@live.se>
2021-11-03 21:38:10 +0100

    Update sortData.tsx

--- util/sortData.tsx
+++ util/sortData.tsx
@@ -3,7 +3,6 @@ import { Coin } from '../api/coinGecko';
 export const sortData = (data: Coin[], sortCat: String) => {
   switch (sortCat) {
     case 'coin': 
-      console.log(' coin triggered')
       data.sort((a, b) => {
         var nameA = a.name[0].toUpperCase();
         var nameB = b.name[0].toUpperCase();
@@ -11,15 +10,12 @@ export const sortData = (data: Coin[], sortCat: String) => {
       });
     break;
     case 'price': 
-      console.log('price triggered')
       data.sort((a, b) => { return  b.current_price-a.current_price; });
       break;
     case '24hr': 
-      console.log('24hr triggered')
       data.sort((a, b) => { return  b.price_change_percentage_24h - a.price_change_percentage_24h; });
       break;
     case 'cap': 
-      console.log('cap triggered')
       data.sort((a, b) => { return b.market_cap - a.market_cap ; });
       break;
     default:
